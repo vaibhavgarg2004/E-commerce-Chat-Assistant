f6aece76a6901094903d10f5632d10ef69390e27
Vaibhav Garg <vaibhavgarg152004@gmail.com>
2025-07-16 02:04:32 +0530

    Updated UI

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = []
 
 for message in st.session_state.messages:
+    
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
